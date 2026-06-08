1e0c693c40e96628c55ebeec014e60bce6838f1c
IMNOTPIZZA <48155178+bbeenkh@users.noreply.github.com>
2026-06-08 10:48:15 +0900

    refactor: stack router 정리

--- src/app/StackRouter.tsx
+++ src/app/StackRouter.tsx
@@ -7,32 +7,20 @@ import LoginPage from '@/pages/login/LoginPage';
 import ProfilePage from '@/pages/profile/ProfilePage';
 import ComponentPlaygroundPage from '@/pages/playground/ComponentPlaygroundPage';
 import GalleryPage from '@/pages/gallery/GalleryPage';
-import useAuthStore from '@/entities/user/model/authStore';
 
 const Stack = createNativeStackNavigator();
 
 export default function StackRouter() {
-  // TODO: 로그인 기능 활성화 시 아래 주석 해제
-  // const accessToken = useAuthStore((state) => state.accessToken);
 
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* {accessToken ? ( */}
-          <>
-            <Stack.Screen name="Todo123" component={TodoPage} />
-            <Stack.Screen name="Noti" component={NotiScreen} />
-            <Stack.Screen name="Profile" component={ProfilePage} />
-            <Stack.Screen name="Gallery" component={GalleryPage} />
-            <Stack.Screen name="Playground" component={ComponentPlaygroundPage} />
-          </>
-        {/* ) : (
-          <Stack.Screen
-            name="Login"
-            component={LoginPage}
-            options={{ headerShown: false }}
-          />
-        )} */}
+        <Stack.Screen name="Todo123" component={TodoPage} />
+        <Stack.Screen name="login" component={LoginPage} />
+        <Stack.Screen name="Noti" component={NotiScreen} />
+        <Stack.Screen name="Profile" component={ProfilePage} />
+        <Stack.Screen name="Gallery" component={GalleryPage} />
+        <Stack.Screen name="Playground" component={ComponentPlaygroundPage} />
       </Stack.Navigator>
     </NavigationContainer>
   )
