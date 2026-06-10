dbbbd40a4434538093e938c6bb0bfc002a855159
bbeenkh <boooong772@naver.com>
2026-06-10 14:58:21 +0900

    feat: sentry.init 추가

--- index.js
+++ index.js
@@ -11,9 +11,21 @@ if (__DEV__) {
 import { AppRegistry } from 'react-native';
 import App from './src/app/App';
 import { name as appName } from './app.json';
+import * as Sentry from "@sentry/react-native";
+import Config from 'react-native-config';
 
 // messaging().setBackgroundMessageHandler(async remoteMessage => {
 //   console.log('Background message:', remoteMessage);
 // });
+
+/**
+ * Sentry config
+ */
+Sentry.init({
+  dsn: Config.SENTRY_DSN,
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+});
 
 AppRegistry.registerComponent(appName, () => App);
