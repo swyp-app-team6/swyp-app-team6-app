9feb9c6c63ca272e7954b847f2b7bec807b774bd
bbeenkh <boooong772@naver.com>
2026-06-15 17:31:33 +0900

    feat: vision-camera peer dependency 추가

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
     '\\.(css|less|scss|sass)$': '<rootDir>/jest.styleMock.js',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|nativewind|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-toast-message|@gorhom|react-native-svg|react-native-worklets|react-native-worklets-core|react-native-css-interop|react-native-config|react-native-encrypted-storage|react-native-image-picker|axios|zustand|immer|@notifee|@testing-library|react-native-qrcode-svg|react-native-vision-camera)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|nativewind|react-native-reanimated|react-native-gesture-handler|react-native-safe-area-context|react-native-screens|react-native-toast-message|@gorhom|react-native-svg|react-native-worklets|react-native-worklets-core|react-native-css-interop|react-native-config|react-native-encrypted-storage|react-native-image-picker|axios|zustand|immer|@notifee|@testing-library|react-native-qrcode-svg|react-native-vision-camera|react-native-nitro-modules|react-native-nitro-image)/)',
   ],
   setupFiles: [
     './node_modules/react-native-gesture-handler/jestSetup.js',
