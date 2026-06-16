6d3802acf9d1bdd97e14701b0cad69da894ce025
bbeenkh <boooong772@naver.com>
2026-06-16 18:10:00 +0900

    feat: 권한 처리 fallback view들 추가

---
+++ src/widgets/permissions/ui/CameraPermissionRequiredFallbackView.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/**
+ * 권한 요구 시 fallback
+ */
+export default function CameraPermissionRequiredFallbackView() {
+  return (
+    <div>CameraPermissionRequiredFallbackView</div>
+  )
+}
