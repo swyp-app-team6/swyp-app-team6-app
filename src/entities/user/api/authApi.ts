dc9d65267f916e4610b3d3917dd79d617d8d8374
bbeenkh <boooong772@naver.com>
2026-06-22 14:40:59 +0900

    chore: 의존성 변경
    - 기존 react-native-app-auth 의존성 제거 및 관련 코드 제거
    - 구글로그인 구현용 @react-native-google-signin/google-signin 추가

--- src/entities/user/api/authApi.ts
+++ src/entities/user/api/authApi.ts
@@ -1,13 +1,6 @@
 import { API } from '@/shared/api';
 import type { User, AuthTokens } from '../model/types';
 
-interface GoogleLoginResponse extends AuthTokens {
-  user: User;
-}
-
-export const googleLogin = (idToken: string) =>
-  API.post<GoogleLoginResponse>('/api/auth/google', { idToken });
-
 export const refreshTokens = (refreshToken: string) =>
   API.post<AuthTokens>('/api/auth/refresh', { refreshToken });
 
