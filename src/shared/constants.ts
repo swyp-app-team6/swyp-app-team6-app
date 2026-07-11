598418e30d0a007f4f6c216926239b0bd142058a
bbeenkh <boooong772@naver.com>
2026-07-11 22:51:43 +0900

    fix: 약관링크: 정식으로변경

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -8,14 +8,14 @@ export const NETWORK_RETRY_COUNT = 2;
 /**
  * 개인정보 처리방침 노션 링크
  */
-export const PRIVACY_URL = 'https://real-jellyfish-10c.notion.site/39144f5610118006bc88d377d1b92aa3';
+export const PRIVACY_URL = 'hhttps://real-jellyfish-10c.notion.site/39444f56101180bcb54fe576bb83b17e?source=copy_link';
 
 /**
  * 이용 약관
  */
-export const SERVICE_URL = 'https://real-jellyfish-10c.notion.site/39144f56101180ab915eed4b439d5233?pvs=74';
+export const SERVICE_URL = 'https://real-jellyfish-10c.notion.site/39444f56101180c4b0cee0382a20c102?source=copy_link';
 
 /**
  * 공지사항
  */
-export const NOTICE_URL = 'https://real-jellyfish-10c.notion.site/39544f56101180efac92fc343e305a1e?source=copy_link';
+export const NOTICE_URL = 'https://real-jellyfish-10c.notion.site/39544f56101180efac92fc343e305a1e?pvs=74';
