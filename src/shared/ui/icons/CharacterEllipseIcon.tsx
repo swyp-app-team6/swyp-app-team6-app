1a4e1cc4da2e5a07fa074c60dc60f7f18819eed2
bbeenkh <boooong772@naver.com>
2026-07-12 21:45:19 +0900

    feat: 캐릭터 후면 그림자아이콘 추가

---
+++ src/shared/ui/icons/CharacterEllipseIcon.tsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Svg, { Circle, Defs, FeBlend, FeFlood, FeGaussianBlur, Filter, G } from 'react-native-svg';
+import type { IconProps } from './types';
+
+/**
+ * # CharacterEllipseIcon
+ * ---
+ * - 간단설명: 가우시안 블러가 적용된 흰색 반투명 원형 배경 아이콘
+ * - 제약사항 및 특이사항:
+ *   - 기본 크기: 288
+ *   - 블러 효과(stdDeviation=30)가 적용된 원형 글로우
+ * ---
+ * @param size 아이콘 크기 (기본 288)
+ * ---
+ * @example
+ * ```tsx
+ * <CharacterEllipseIcon />
+ * <CharacterEllipseIcon size={200} />
+ * TODO: 제대로된 아이콘 나오면 대체
+ * ```
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function CharacterEllipseIcon({ size = 288 }: IconProps) {
+  return null;
+}
